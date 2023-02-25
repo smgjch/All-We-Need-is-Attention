b27fe62cd387d9286e07df351b6dc3421f5c0ff8
cyt <cytseung20@gmail.com>
2023-02-25 22:56:34 +0000

    image preview

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ import axios from "axios";
 
 import "./App.css";
 
-// import Button from "@mui/material/Button";
+import Button from "@mui/material/Button";
 // import { Container, Box, Grid, TextField, FormControl } from "@mui/material";
 import { styled } from "@mui/material/styles";
 
@@ -22,14 +22,15 @@ function App() {
   });
 
   const [selectedFile, setSelectedFile] = React.useState(null);
+  const [image, setImage] = React.useState(null);
 
     const onFileChange = (e) => {
       setSelectedFile(e.target.files[0]);
+      setImage(URL.createObjectURL(e.target.files[0]));
   }
   const onFileUpload = async () => {
       const formData = new FormData();
       formData.append('file', selectedFile, selectedFile.name);
-      // POST request here
       await fileUploadToBackEnd(formData);
   }
   const fileData = () => {
@@ -38,7 +39,7 @@ function App() {
             <div>
                 <p>File Name: {selectedFile.name}</p>
                 <p>File Type: {selectedFile.type}</p>
-
+                <img src={image}  width="500"  alt="The current file" />
             </div>
         )
     }
@@ -47,25 +48,25 @@ function App() {
   return (
     <>
       <label htmlFor="choose-file-button">
-        <input
+        <Input
           id="choose-file-button"
           type="file"
           onChange={onFileChange}
         />
-        <button variant="contained" component="span">
+        <Button variant="contained" component="span">
           Choose File
-        </button>
+        </Button>
       </label>
       {fileData()}
       {selectedFile && (
         // <Grid component="span" item m={3}>
-          <button
+          <Button
             variant="contained"
             sx={{ position: "inherit", right: "10%", bottom: "10%" }}
             onClick={onFileUpload}
           >
             Upload
-          </button>
+          </Button>
         // </Grid>
       )}
     </>
